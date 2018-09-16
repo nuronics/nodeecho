8670f2f6a80eb5504a6d370a52aca8ad655d964e
nuronics <42369707+nuronics@users.noreply.github.com>
2018-09-17 00:15:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,17 +29,6 @@ restService.post("/echo", function(req, res) {
   });
 });
 
-
-  return res.json({
-    speech: "speech",
-    displayText: "speech",
-    source: "webhook-echo-sample",
-    data: {
-      slack: slack_message
-    }
-  });
-});
-
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
