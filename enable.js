b29f4301f6bc404e6396b248bdb38dbde8d1ff5d
nuronics <42369707+nuronics@users.noreply.github.com>
2018-09-17 01:27:10 +0530

    Update enable.js

--- enable.js
+++ enable.js
@@ -1,3 +1,41 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const DialogflowApp = require('actions-on-google').DialogflowApp;
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+    
+    const requestPermission = (app) => {
+      app.askForPermission('To locate you', app.SupportedPermissions.DEVICE_PRECISE_LOCATION);
+    };
+    
+    const userInfo = (app) => {
+        if (app.isPermissionGranted()) {
+            const address = app.getDeviceLocation().address;
+            if (address) {            
+                app.tell(`You are at ${address}`);
+            }
+            else {
+                // Note: Currently, precise locaton only returns lat/lng coordinates on phones and lat/lng coordinates 
+                // and a geocoded address on voice-activated speakers. 
+                // Coarse location only works on voice-activated speakers.
+                app.tell('Sorry, I could not figure out where you are.');
+            }
+        } else {
+            app.tell('Sorry, I could not figure out where you are.');
+        }
+    };
+
+    const app = new DialogflowApp({request, response});
+    const actions = new Map();
+    actions.set('request_permission', requestPermission);
+    actions.set('user_info', userInfo);
+    app.handleRequest(actions);
+
+
+
+
+
 app.intent('ask_for_place', (conv) => {
   conv.ask(new Place(options));
 });
